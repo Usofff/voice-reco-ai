528ff50e8feb9eda50fea8f2cdef7aa8609e9985
Ilya Usov <iusov41@gmail.com>
2025-08-16 23:06:08 +0300

    init: start project

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,5 @@
+export default function NewCompanion() {
+  return (
+    <div>NewCompanion</div>
+  )
+}
---
+++ app/subscribtion/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Subscribtion() {
+  return (
+    <div>Subscribtion</div>
+  )
+}
