94da7fa2af2f64ec2e2dc00cc56d5906bb43f06e
Ilya Usov <iusov41@gmail.com>
2025-08-17 18:56:08 +0300

    feat: add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import NavItems from "./NavItems";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link href="/">
+        <div className="flex item-center gap-2.5 cursor-pointer">
+          <Image src="/images/logo.svg" alt="" width={46} height={44} />
+        </div>
+      </Link>
+      <div className="flex items-center gap-8">
+        <NavItems />
+        <p>Sign in</p>
+      </div>
+    </nav>
+  );
+}
