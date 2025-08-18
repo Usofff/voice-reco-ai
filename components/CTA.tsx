f8ad48918e22060db6655915a0682713883c2232
Ilya Usov <iusov41@gmail.com>
2025-08-18 07:28:58 +0300

    feat: add cta and CompanionsList to home page

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
 
 export default function Cta() {
   return (
-    <div>CTA</div>
-  )
+    <section className="cta-section">
+      <div className="cta-badge">Start learning your way.</div>
+      <h2 className="text-3xl font-bold">
+        Build a Personalize Learning Companion
+      </h2>
+      <p>
+        Pick a name, subject, voice, & personality — and start learning through
+        voice conversations that feel natural and fun.
+      </p>
+      <Image src={"/images/cta.svg"} alt="cta" width={362} height={232} />
+      <button className="btn-primary">
+        <Image src={"/icons/plus.svg"} alt="plus" width={12} height={12} />
+        <Link href="/companions/new">
+          <p>Build New Companion</p>
+        </Link>
+      </button>
+    </section>
+  );
 }
