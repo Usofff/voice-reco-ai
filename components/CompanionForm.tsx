d6eb4ce7bb0a27fd128c9c6318a9bf5ef1e050f2
Ilya Usov <iusov41@gmail.com>
2025-08-20 21:57:11 +0300

    feat: implement companion creating logic

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -25,6 +25,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { subjects } from "@/constants";
 import { Textarea } from "./ui/textarea";
+import { createCompanion } from "@/lib/actions/companion.actions";
+import { redirect } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Companion is required" }),
@@ -50,8 +52,14 @@ export default function CompanionForm() {
     },
   });
 
-  const onSubmit = (values: CompanionFormValues) => {
-    console.log(values);
+  const onSubmit = async (values: CompanionFormValues) => {
+    const companion = await createCompanion(values)
+    if(companion){
+       redirect(`/companions/${companion.id}`)
+    } else {
+      console.log('Failed to create a companion');
+      redirect(`/`)
+    }
   };
 
   return (
