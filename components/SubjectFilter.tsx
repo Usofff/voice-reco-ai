1a3b88cb020859db4737c7c1de3cac9c3b4e66ee
Ilya Usov <iusov41@gmail.com>
2025-08-21 07:23:27 +0300

    feat: implement filtering options for compations library

--- components/SubjectFilter.tsx
+++ components/SubjectFilter.tsx
@@ -1,9 +1,49 @@
+"use client";
 
+import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { subjects } from "@/constants";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@jsmastery/utils";
 
 const SubjectFilter = () => {
-  return (
-    <div>SubjectFilter</div>
-  )
-}
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("topic") || "";
 
-export default SubjectFilter
+  const [selectedQuery, setselectedQuery] = useState("");
+  const handleSelectChange = (value: string) => {
+    setselectedQuery(value);
+      console.log(1)
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "subject",
+        value: value,
+      });
+      router.push(newUrl, { scroll: false });
+  };
+
+  return (
+    <Select onValueChange={handleSelectChange} value={selectedQuery}>
+      <SelectTrigger className="input">
+        <SelectValue placeholder="Select the voice" />
+      </SelectTrigger>
+      <SelectContent>
+        {subjects.map((subject) => (
+          <SelectItem value={subject} key={subject}>
+            {subject}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default SubjectFilter;
