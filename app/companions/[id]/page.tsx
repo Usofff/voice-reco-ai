51cb2ab0dbdc0916b94f0f12a11adf0a49e8bb65
Ilya Usov <iusov41@gmail.com>
2025-08-22 07:26:08 +0300

    feat: add vapi & create companion session header

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import { getCompanion } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function CompanionSession() {
-  return (
-    <div>CompanionSession</div>
-  )
+interface CompanionSessionPageProps {
+  params: Promise<{ id: string }>;
 }
+
+const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
+  const { id } = await params;
+  const companion = await getCompanion(id);
+  const user = await currentUser();
+
+  if (!user) redirect("/sign-in");
+
+  if (!companion) redirect("/companions");
+
+  return (
+    <main>
+      <article className="flex rounded-border justify-between p-6 max-md:flex-col">
+        <div className="flex items-center gap-2">
+          <div
+            className="size-[72px] flex items-center justify-center rounded-lg max-md:hodden"
+            style={{
+              backgroundColor: getSubjectColor(companion.subject),
+            }}
+          >
+            <Image
+              alt={`${companion.subject}`}
+              src={`/icons/${companion.subject}.svg`}
+              width={35}
+              height={35}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-2">
+              <p className="font-bold text-2xl">{companion.name}</p>
+              <div className="subject-badge max-sm:hidden">
+                {companion.subject}
+              </div>
+            </div>
+            <p className="text-lg">{companion.topic}</p>
+          </div>
+        </div>
+        <div className="items-start text-2xl max-md:hidden">{companion.duration} minutes</div>
+      </article>
+    </main>
+  );
+};
+
+export default CompanionSession;
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -29,10 +29,20 @@ export const getAllCompanions = async ({ limit = 10, page = 1, subject, topic }:
         query = query.or(`topic.ilike.%${topic},name.ilike.%${topic}`)
     }
 
-    query = query.range((page -1 ) * limit, page * limit - 1)
+    query = query.range((page - 1) * limit, page * limit - 1)
 
-    const {data: companions, error} = await query;
+    const { data: companions, error } = await query;
 
     if (error) throw new Error(error.message);
     return companions;
+}
+
+export const getCompanion = async (id: string) => {
+    const supabase = createSupabaseClient();
+    const {data, error} = await supabase.from('companions').select().eq('id', id)
+
+    if(error) {
+        return console.log(error)
+    }
+    return data[0];
 }
