aeb0a62472123f092036ece85fe4df96bdbf1455
Ilya Usov <iusov41@gmail.com>
2025-08-26 08:01:39 +0300

    feat: add conversation Session page UI

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -26,6 +26,7 @@ const CompanionComponent = ({
   const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
   const [isSpeaking, setIsSpeaking] = useState(false);
   const lotteiRef = useRef<LottieRefCurrentProps>(null);
+  const [isMuted, setIsMuted] = useState(false);
 
   useEffect(() => {
     if (lotteiRef) {
@@ -69,6 +70,20 @@ const CompanionComponent = ({
     };
   }, []);
 
+  const toggleMicrophone = () => {
+    const isMuted = vapi.isMuted();
+    vapi.setMuted(!isMuted);
+    setIsMuted(!isMuted);
+  };
+  
+  const handleDisconnect = () => {
+
+  }
+
+  const handleCall = async () => {
+
+  }
+
   return (
     <section className="flex flex-col h-[70vh]">
       <section className="flex gap-8 max-sm:flex-col">
@@ -111,7 +126,50 @@ const CompanionComponent = ({
               </div>
             </div>
           </div>
+          <p className="font-bold text-2xl">{name}</p>
         </div>
+        <div className="user-section">
+          <div className="user-avatar">
+            <Image
+              alt="user"
+              src={userImage}
+              width={130}
+              height={130}
+              className="rounded-lg"
+            />
+            <p className="font-bold text-2xl">{userName}</p>
+          </div>
+          <button className="btn-mic" onClick={toggleMicrophone}>
+            <Image
+              src={isMuted ? "/icons/mic-off.svg" : "/icons/mic-on.svg"}
+              alt="mic"
+              width={36}
+              height={36}
+            />
+            <p className="max-sm:hidden">
+              {isMuted ? "Turn on Microphone" : "Turn off microphone"}
+            </p>
+          </button>
+          <button
+            className={cn(
+              "rounded-lg py-2 cursor-pointer transition-colors w-full text-white",
+              callStatus === CallStatus.ACTIVE ? "bg-red-700" : "bg-primary",
+              callStatus === CallStatus.CONNECTING && "animate-pulse"
+            )}
+            onClick={
+              callStatus === CallStatus.ACTIVE ? handleDisconnect : handleCall
+            }
+          >
+            {callStatus === CallStatus.ACTIVE
+              ? "End Session"
+              : callStatus === CallStatus.CONNECTING
+              ? "Connecting"
+              : "Start Session"}
+          </button>
+        </div>
+      </section>
+      <section className="transcript">
+
       </section>
     </section>
   );
