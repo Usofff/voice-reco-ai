f97d7791f88272236a5c6daea2077eef155e601b
Ilya Usov <iusov41@gmail.com>
2025-08-28 17:24:10 +0300

    feat: implement sessions history

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -5,6 +5,7 @@ import Lottie, { LottieRefCurrentProps } from "lottie-react";
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
 import soundwaves from "@/constants/soundwaves.json";
+import { addToSessionHistory } from "@/lib/actions/companion.actions";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -45,6 +46,7 @@ const CompanionComponent = ({
     };
     const onCallEnd = () => {
       setCallStatus(CallStatus.FINISHED);
+      addToSessionHistory(companionId)
     };
     const onMessage = (message: Message) => {
       if (message.type === "transcript" && message.transcriptType === "final") {
